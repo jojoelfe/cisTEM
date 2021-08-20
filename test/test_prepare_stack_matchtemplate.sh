304b28022dda09692f1f834c5175efefa2d09111
Johannes Elferich <johannes.elferich@umassmed.edu>
2021-08-20 10:59:17 -0400

    Intial tests

---
+++ test/test_prepare_stack_matchtemplate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+~/cisTEM_downstream_bah/build/custom_starfile/src/prepare_stack_matchtemplate << eof
+No
+/scratch_bern/elferich/ER_HoxB8_144h/Scratch/Template_Match/s_records_50_00000_-20.0_30_0_30_scaled_mip.mrc
+/scratch_bern/elferich/ER_HoxB8_144h/Scratch/Template_Match/s_records_50_00000_-20.0_30_0_30_psi.mrc
+/scratch_bern/elferich/ER_HoxB8_144h/Scratch/Template_Match/s_records_50_00000_-20.0_30_0_30_theta.mrc
+/scratch_bern/elferich/ER_HoxB8_144h/Scratch/Template_Match/s_records_50_00000_-20.0_30_0_30_phi.mrc
+/scratch_bern/elferich/ER_HoxB8_144h/Scratch/Template_Match/s_records_50_00000_-20.0_30_0_30_defocus.mrc
+/scratch_bern/elferich/ER_HoxB8_144h/Assets/ParticleStacks/data/s_records_50_00000_-20.0_30_0_30coordinates.txt
+7.7
+10.0
+1
+/scratch_bern/elferich/ER_HoxB8_144h/Assets/Images/s_records_50_00000_-20.0_30_0.mrc
+test.star
+/scratch_bern/elferich/ER_HoxB8_144h/Assets/ParticleStacks/data/s_records_50_00000_-20.0_30_0_30stack.mrc
+384
+1.5
+6622.53759765625
+6390.302734375
+-46.831695556640625
+300
+2.7
+0.07
+eof
