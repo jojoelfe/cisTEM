f56809e753a7178db54a6961770c1d24986f21e2
Johannes Elferich <johannes.elferich@umassmed.edu>
2021-11-06 13:54:27 -0400

    crop_and_add_noise

--- src/core/image.h
+++ src/core/image.h
@@ -429,6 +429,8 @@ public:
 	void SetMinimumAndMaximumValues( float new_minimum_value, float new_maximum_value);
 	void Binarise(float threshold_value);
 	void BinariseInverse(float threshold_value);
+	void CropOutDarkAreas(float sigma_for_filter = 0.01, float threshold_percentile=0.1);
+	std::tuple<int, int> CropAndAddGaussianNoiseToDarkAreas(float sigma_for_filter = 0.01, float threshold_percentile = 0.1, bool calc_sigma_mean = true, float sigma_for_noise = 1.0, float mean_for_noise = 0.0);
 
 
 	void ComputeAverageAndSigmaOfValuesInSpectrum(float minimum_radius, float maximum_radius, float &average, float &sigma, int cross_half_width = 2);
